cd20fd6b1c4e35662e423996d9fb79d8f4a5e516
VladislavHavryliuk <78643176+VladislavHavryliuk@users.noreply.github.com>
2024-03-11 13:26:35 +0200

    Сreated a custom logger

---
+++ Logger.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace QuickMark
+{
+    public static class Logger
+    {
+        private static string logFilePath;
+
+        static Logger()
+        {
+            string filepath = "log.txt";
+            logFilePath = filepath;
+        }
+
+        public static void Log(string message)
+        {
+            string logMessage = $"[{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")}] {message}";
+            File.AppendAllText(logFilePath, logMessage + Environment.NewLine);
+        }
+    }
+}
