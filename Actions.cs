88d2787a593db98c97471dd37d58dc66b8f660b0
VladislavHavryliuk <78643176+VladislavHavryliuk@users.noreply.github.com>
2024-03-11 13:35:39 +0200

    procurement/referral methods

---
+++ Actions.cs
@@ -0,0 +1,15 @@
+﻿namespace QuickMark
+{
+    public static class Actions
+    {
+        public static void OrderProduct(int id, int count, int storageId)
+        {
+            // Логіка для методу OrderProduct компанії QuickMark
+        }
+
+        public static void RedirectProduct(int id, int count, int storageId)
+        {
+            // Логіка для методу RedirectProduct компанії QuickMark
+        }
+    }
+}
